44a8ebdecadd1decb543be7ae3714c71d1cc61e9
Syed Intiser Ahsan <Intiser@users.noreply.github.com>
2024-10-22 17:58:08 +0200

    layout update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,18 @@ import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { TopbarComponent } from './layout/topbar/topbar.component';
 import { MainComponent } from './layout/main/main.component';
+import { LandingPageComponent } from './pages/landing-page/landing-page.component';
+import { MembersDetailsComponent } from './pages/members-details/members-details.component';
+import { VisionDetailsComponent } from './pages/vision-details/vision-details.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TopbarComponent,
-    MainComponent
+    MainComponent,
+    LandingPageComponent,
+    MembersDetailsComponent,
+    VisionDetailsComponent
   ],
   imports: [
     BrowserModule,
